32c959da58d29d0da4168a0b0a161436955ba47b
Josh Luria <josh.luria@gmail.com>
2017-12-08 10:51:58 -0500

    Define student model

---
+++ server/db/models/students.js
@@ -0,0 +1,21 @@
+const Sequelize = require('sequelize');
+const db = require('../db');
+
+module.exports = db.define('student', {
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true
+    }
+  },
+  gpa: Sequelize.FLOAT(1, 1)
+});
